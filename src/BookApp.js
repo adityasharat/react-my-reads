0d61b7bb49a39b1bd08ed2f0b0ce5d21b4992f2e
Aditya Sharat <aditya.sharat@flipkart.com>
2017-07-05 00:23:24 +0530

    changes to library component

--- src/BookApp.js
+++ src/BookApp.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import * as BooksAPI from './BooksAPI';
-import './App.css';
+import './BookApp.css';
 import { Route } from 'react-router-dom';
 import { Toolbar } from './Toolbar.js';
 import { Library } from './Library.js';
 
 class BooksApp extends React.Component {
   state = {
+    query: '',
+    books: []
   };
 
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        console.error(books);
+        this.setState({ books });
+      });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <Route path="/" render={() => {
           return (
             <div>
               <Toolbar/>
-              <Library/>
+              <Library books={this.state.books}/>
             </div>
           )
         }}
--- src/Library.js
+++ src/Library.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class Library extends React.Component {
   render() {
-    return (<h2>Library will be here</h2>);
+    console.log(this.props);
+    return (
+      <div>
+        <ul>
+          {this.props.books.map((book) => {
+            return <li key={book.id}>{book.title}</li>
+          })}
+        </ul>
+      </div>);
   }
 }
 
